2e2e9d25126e44198ce0077f2f2aa5d74cf7e9b1
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-13 17:05:00 +0000

    start destructuring coding ES6

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,31 @@
  * command node destructuring.js in the terminal
  */
 
-// Destructuring arrays
+// Destructuring Arrays
 
-// Destructuring objects
+let ages = [30, 26, 27];
+let [john, mary, joe] = ages;
+
+console.log(`John: ${john}\nMary: ${mary}\nJoe: ${joe}\n`);
+console.log('-------------');
+
+// Destructuring Objects
+
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant"
+};
+
+let { mike, jill, alicia } = jobs;
+
+console.log(`Mike: ${mike}\nJill: ${jill}\nAlicia: ${alicia}\n`);
+console.log('-------------');
 
 // Destructuring subsets
+let languages = ['English', 'French', 'Spanish', 'German', 'Japanese'];
+let [, , , johnNative, johnSecundary] = languages;
 
+console.log(johnNative, johnSecundary);
+console.log('-------------');
 // Using rest parameter syntax
---
+++ vanilla.js
@@ -0,0 +1,33 @@
+// Vanilla JS Array
+
+let ages = [30, 26, 27];
+let john = ages[0];
+let mary = ages[1];
+let joe = ages[2];
+
+console.log(`John: ${john}\nMary: ${mary}\nJoe: ${joe}`);
+console.log('-------------');
+
+// Vanilla JS Object
+
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant"
+};
+
+let mike = jobs.mike;
+let jill = jobs.jill;
+let alicia = jobs.alicia;
+
+console.log(`Mike: ${mike}\nJill: ${jill}\nAlicia: ${alicia}`);
+console.log('-------------');
+// Vanilla JS Subsets
+
+let languages = ['English', 'French', 'Spanish', 'German', 'Japanese'];
+
+let johnNative = languages[0];
+let johnSecundary = languages[1];
+
+console.log(`John First Language: ${johnNative}\nJohn Second Language: ${johnSecundary}`);
+console.log('-------------');
