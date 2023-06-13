7f03eff83ce36765cf34d8536a5b44d4c732d0ef
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-13 20:30:29 +0000

    finish destructuring

---
+++ practice.js
@@ -0,0 +1,48 @@
+let students = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const makeList = (arr, student) => {
+    let studentSubjects;
+    for (target of arr){
+        if (target.name === student){
+            studentSubjects = target.subjects;
+        }
+    }
+    return studentSubjects
+}
+
+// This
+
+let myStudent = makeList(students, 'John');
+
+let [first, second, ...rest] = myStudent;
+
+//OR
+
+// let [first, second, ...rest] = makeList(students, 'John');
+
+console.log (first, second, rest);
